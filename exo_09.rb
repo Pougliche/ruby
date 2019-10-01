7291c857c11b8c3cfe75b31b171a6267baa76714
Pougliche <pugliese.viktor@gmail.com>
2019-10-01 20:10:56 +0200

    exo > 14

--- exo_09.rb
+++ exo_09.rb
@@ -2,5 +2,5 @@ puts "Bonjour, quel est ton prénom?"
 user_name = gets.chomp
 puts "Quel est ton nom de famille ?"
 user_surname = gets.chomp
-print "Salutations, " , user_name + user_surname
+print "Salutations, " , user_name   +   user_surname
 puts 
---
+++ exo_12.rb
@@ -0,0 +1,7 @@
+puts "Your birth year"
+nombre =gets.to_i
+i = 2019
+while (nombre < i)
+   puts nombre
+           nombre = nombre + 1
+end
