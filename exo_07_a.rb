66e2dbee5e4dc51a0be763c03e5e680ee04f201d
Pougliche <pugliese.viktor@gmail.com>
2019-10-01 16:01:33 +0200

    exo 7abc

---
+++ exo_07_a.rb
@@ -0,0 +1,5 @@
+puts "Bonjour, c'est quoi ton blase ?"
+user_name = gets.chomp
+puts user_name
+
+#gets.chomp permet de demander des informations à l'utilisateur qui seront ensuite enregistrées dans le programme (puts user name ensuite renseigne l'info input par l'utilisateur à la ligne d'avant)
---
+++ exo_07_c.rb
@@ -0,0 +1,7 @@
+user_name = gets.chomp
+puts user_name
+
+#Différence entre les trois programmes : 
+#Le premier demande simplement le nom d'utilisateur et le répète
+#Le second utilise la commande print pour ajouter une ligne ">" qui indique visuellement à l'utilisateur d'entrer son nom, puis le répète. 
+#Le troisième ne propose aucune interface, l'utilisateur doit devenir qu'il doit rentrer son nom (mais il est tout de même répété ensuite)
